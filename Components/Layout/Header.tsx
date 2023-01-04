b46be61206e755c1fb694f7502b04a92ce989d45
ROHIT KUMAR TIWARI <72463795+Rohitkumartiwari@users.noreply.github.com>
2023-01-04 12:23:01 +0530

    Icons

--- Components/Layout/Header.tsx
+++ Components/Layout/Header.tsx
@@ -1,15 +1,31 @@
 import React from 'react'
 import img from "../../public/images/iid-logo.png"
-import Image from 'next/image'
+import Image from 'next/image';
+import Link from "next/link"
+import { FaAngleDown } from "react-icons/fa";
 const Header = () => {
     return (
-        <div className="container py-[18px]">
-            <div className="flex justify-between ...">
+        <>
+            <div className="container py-[18px]">
+                <div className="flex justify-between ...">
 
-                <Image src={img} alt=""  />
-                <div>03</div>
+                    <Image src={img} alt="" />
+                    <div className="flex space-x-10 items-center">
+                        <div className="flex space-x-4 cursor-pointer text-gray">
+                            <span>
+
+                                All Services
+                            </span>
+                            <span className=' flex items-center' ><FaAngleDown /></span>
+                        </div>
+                        <Link className=" cursor-pointer text-gray" href="#">Courses</Link>
+                        <Link className=" cursor-pointer text-gray" href="#">  About us</Link>
+                        <Link className=" cursor-pointer text-gray" href="#">My Account</Link>
+                    </div>
+                </div>
             </div>
-        </div>
+
+        </>
     )
 }
 
