d2b9ec9c7260bc4eb341d89a9cc66b9890717a2b
ROHIT KUMAR TIWARI <72463795+Rohitkumartiwari@users.noreply.github.com>
2023-01-04 11:18:32 +0530

    Logo

--- Components/Layout/Header.tsx
+++ Components/Layout/Header.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import img from "../../public/images/iid-logo.png"
+import Image from 'next/image'
 const Header = () => {
     return (
         <div className="container py-[18px]">
-            Header
+            <div className="flex justify-between ...">
+
+                <Image src={img} alt=""  />
+                <div>03</div>
+            </div>
         </div>
     )
 }
