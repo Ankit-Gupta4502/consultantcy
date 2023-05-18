ebeadf98092878258f844345709cfc8bf773c1fe
ankit_gupta <ankitgupta2224@gmail.com>
2023-05-18 12:07:53 +0530

    imported tailwind css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import Layout from '../Components/Layout/Layout'
 import { Provider } from 'react-redux'
 import 'react-multi-carousel/lib/styles.css'
 import 'react-toastify/dist/ReactToastify.css';
+import 'tailwindcss/tailwind.css'
 export default function App({ Component, pageProps }: AppProps) {
   return <Provider store={useStore(pageProps.initialReduxState)}>
     <Layout>
